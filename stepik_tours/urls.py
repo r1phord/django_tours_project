eda0cff1d2af610a30da1d2baa6ddff1e5bd7032
r1phord <vlad.gorenkov.1996@mail.ru>
2020-06-30 09:17:11 +0200

    add named urls

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -6,7 +6,7 @@ handler404 = custom_handler404
 handler500 = custom_handler500
 
 urlpatterns = [
-    path('', MainView.as_view()),
-    path('departure/<str:departure>/', DepartureView.as_view()),
-    path('tour/<int:id>/', TourView.as_view()),
+    path('', MainView.as_view(), name='main'),
+    path('departure/<str:departure>/', DepartureView.as_view(), name='departure'),
+    path('tour/<int:id>/', TourView.as_view(), name='tour'),
 ]
